13d4b46d9cb96a21519617eecb64631907d6c20c
zecoziyad <zecoziyad@gmail.com>
2022-10-29 15:55:25 -0400

    .

--- rps.js
+++ rps.js
@@ -1,68 +1,54 @@
-let playerScore = 0;
-  let computerScore = 0;
+function computerPlay() {
+    let choices = ['rock', 'paper', 'scissors']
+    return choices[Math.floor(Math.random() * choices.length)]
+}
 
-function getComputerChoice(){
-    let randomChoice = ['rock','paper','scissors'];
-    return randomChoice[Math.floor(Math.random() *randomChoice.length)]
-        
-}
-function playerChoice(userInput){
-   if(userInput==='rock'){
-    return 'rock'
-   }else if(userInput==='paper'){
-    return  'paper'
-   }else if(userInput==='scissors'){
-    return 'scissors'
-   }else {
-    return 'EROOOR' 
-   }
-    
-}
-let result = '';
 function playRound(playerSelection, computerSelection) {
-    // your code here!
-    let result = "";
+    let playerPoint = 0
+
     if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
         (playerSelection == 'scissors' && computerSelection == 'paper') ||
-        (playerSelection == 'paper' && computerSelection == 'rock')) {
-        
-        playerScore += 1
-        result = ('You win! ' + playerSelection + ' beats ' + computerSelection
-            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
-
-        if (playerScore == 5) {
-            result += '<br><br>You won the game! Reload the page to play again'
-            
-        }
-    }
-    else if (playerSelection == computerSelection) {
-        result = ('It\'s a tie. You both chose ' + playerSelection
-            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+        (playerSelection == 'paper' && computerSelection == 'rock'))
+    {
+        playerPoint = 1
+        console.log('You win! ' + playerSelection + ' beats ' + computerSelection)
     }
     else {
-        computerScore += 1
-        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
-            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+        console.log('You lose! ' + computerSelection + ' beats ' + playerSelection)
+    }
 
-        if (computerScore == 5) {
-            result += '<br><br>I won the game! Reload the page to play again'
-            
-        }
-    } return result
+    return playerPoint
 }
-    
-  
-  
-  
 
-   
-  const playerSelection = playerChoice('rock');
-  const computerSelection = getComputerChoice();
-  console.log(playerSelection);
-  console.log(computerSelection);
-  console.log(playRound(playerSelection, computerSelection));
+function game(roundsTotal = 5) {
+    let roundsPlayed = 0
+    let playerScore = 0
 
- 
+    while (roundsPlayed < roundsTotal) {
+        let playerSelection = prompt('rock, paper, or scissors?')
+        playerSelection = playerSelection.toLowerCase();
+        if ((playerSelection != 'rock') &&
+            (playerSelection != 'paper') &&
+            (playerSelection != 'scissors'))
+        { continue }
 
- 
+        let computerSelection = computerPlay()
+        if (playerSelection == computerSelection) {
+            console.log('It\'s a tie. You both chose ' + playerSelection)
+            continue
+        }
 
+        playerScore += playRound(playerSelection, computerSelection)
+        roundsPlayed++
+    }
+
+    if (playerScore > (roundsPlayed / 2)) {
+        console.log('You won ' + playerScore + ' out of ' + roundsPlayed + ' rounds. You win!')
+    }
+    else if (playerScore == (roundsPlayed / 2)) {
+        console.log('You won ' + playerScore + ' out of ' + roundsPlayed + ' rounds. You tied.')
+    }
+    else {
+        console.log('You won ' + playerScore + ' out of ' + roundsPlayed + ' rounds. You lost!')
+    }
+}
